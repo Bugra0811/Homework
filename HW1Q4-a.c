80b57d425acc43af9359759542254174145b6598
Bugra0811 <bugrakoyun18@gmail.com>
2025-03-23 16:05:59 +0300

    Add files via upload

---
+++ HW1Q4-a.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+void computehomevalue(int popularity, int size, int *homevalue);  
+
+int main()
+{
+    int popularity,size,homevalue;
+      
+    printf(" Please enter popularity:");
+    scanf("%d",&popularity);
+
+    printf("Please enter size:");
+    scanf("%d",&size); 
+
+    computehomevalue(popularity,size,&homevalue); 
+    printf("Home value is: %d\n",homevalue); 
+
+    return 0;
+}
+
+void computehomevalue(int popularity,int size,int *homevalue) 
+{
+    int sizepow = pow(size,2);        
+    int popularitypow = pow(popularity,3);  
+
+    *homevalue = (popularitypow + sizepow) * 10000;
+	return 0;
+}
