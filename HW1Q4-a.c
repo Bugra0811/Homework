4b9415ddc7f8a449d25077faafda94222fb56fd8
Bugra0811 <bugrakoyun18@gmail.com>
2025-03-23 19:30:35 +0300

    Add files via upload

--- HW1Q4-a.c
+++ HW1Q4-a.c
@@ -1,62 +1,29 @@
-[Project]
-FileName=HW1Q4-a.dev
-Name=Project1
-Type=1
-Ver=2
-ObjFiles=
-Includes=
-Libs=
-PrivateResource=
-ResourceIncludes=
-MakeIncludes=
-Compiler=
-CppCompiler=
-Linker=
-IsCpp=0
-Icon=
-ExeOutput=
-ObjectOutput=
-LogOutput=
-LogOutputEnabled=0
-OverrideOutput=0
-OverrideOutputName=
-HostApplication=
-UseCustomMakefile=0
-CustomMakefile=
-CommandLine=
-Folders=
-IncludeVersionInfo=0
-SupportXPThemes=0
-CompilerSet=0
-CompilerSettings=0000000000000000000000000
-UnitCount=1
+#include <stdio.h>
+#include <math.h>
 
-[VersionInfo]
-Major=1
-Minor=0
-Release=0
-Build=0
-LanguageID=1033
-CharsetID=1252
-CompanyName=
-FileVersion=
-FileDescription=Developed using the Dev-C++ IDE
-InternalName=
-LegalCopyright=
-LegalTrademarks=
-OriginalFilename=
-ProductName=
-ProductVersion=
-AutoIncBuildNr=0
-SyncProduct=1
+int computehomevalue(int popularity, int size);  
 
-[Unit1]
-FileName=HW1Q4-a.c
-CompileCpp=0
-Folder=
-Compile=1
-Link=1
-Priority=1000
-OverrideBuildCmd=0
-BuildCmd=
+int main()
+{
+    int popularity,size,homevalue;
+     
+    printf("Please enter popularity: ");
+    scanf("%d", &popularity);
 
+    printf("Please enter size: ");
+    scanf("%d", &size); 
+
+    homevalue = computehomevalue(popularity,size); 
+    printf("Home value is: %d\n", homevalue); 
+
+    return 0;
+}
+
+
+int computehomevalue(int popularity, int size) 
+{
+    int sizepow = pow(size,2);        
+    int popularitypow = pow(popularity,3);  
+	
+    return (popularitypow + sizepow)*10000; 
+}
